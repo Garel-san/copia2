d0e4dd2cf82b2a8e018925a27495046ab033e8f3
unknown <ivanrealgnr2000@gmail.com>
2025-12-03 09:29:49 -0300

    Integración backend + validaciones + fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Form } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Section1 from "./pages/Section1";
 import Section2 from "./pages/Section2";
